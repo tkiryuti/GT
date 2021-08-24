2fa6b5e369970f28b1e16ef8194cd0fb28a6691a
Tanya Kiryutina <46792196+tkiryuti@users.noreply.github.com>
2021-08-24 17:57:50 -0400

    Update delete-jobs.sh

--- PACE-cluster-utility-scripts/delete-jobs.sh
+++ PACE-cluster-utility-scripts/delete-jobs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Deletes all jobs in the cluster queue that are status R (running) and Q (queued) using xargs
 qstat -u $(whoami) | grep -e '\sR\s' -e '\sQ\s' | awk '{print $1}' | sed 's/\..*//' | xargs qdel
-# qstat -u $(whoami) | grep -e '\sR\s' -e '\sQ\s' | awk '{print $1}' | xargs -I ID qdel IDpace.gatech.edu
